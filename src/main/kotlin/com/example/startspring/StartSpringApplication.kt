89088f5063b95d66d56d72aeb4bcca31e5cbb4d7
전승원 <78971821+louis7308@users.noreply.github.com>
2023-10-06 14:33:37 +0900

    [Setting]:  Project 초기 세팅

---
+++ src/main/kotlin/com/example/startspring/StartSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.startspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<StartSpringApplication>(*args)
+}

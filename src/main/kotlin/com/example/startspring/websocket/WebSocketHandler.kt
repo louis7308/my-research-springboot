a660a38f01fb06dae5f12d2bf0303b1edb4f2d1a
전승원 <78971821+louis7308@users.noreply.github.com>
2023-10-10 10:47:01 +0900

    [Add]: afterConnection 로직 작성

--- src/main/kotlin/com/example/startspring/websocket/WebSocketHandler.kt
+++ src/main/kotlin/com/example/startspring/websocket/WebSocketHandler.kt
@@ -1,6 +1,48 @@
 package com.example.startspring.websocket
 
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.util.concurrent.ConcurrentHashMap
 
 class WebSocketHandler : TextWebSocketHandler() {
+    private val sessions = ConcurrentHashMap<String, WebSocketSession>()
+
+    // 웹소켓 연결
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        var sessionId = session.id
+        sessions[sessionId] = session
+
+        val message: Message = Message(sender = sessionId, receiver = "all")
+        message.newConnect()
+
+        sessions.values
+            .filter { it.id != sessionId }
+            .forEach { otherSession ->
+                run {
+                    try {
+                        otherSession.sendMessage(TextMessage("Welcome new user $sessionId"))
+                    } catch (e: Exception) {
+                        throw RuntimeException("")
+                    }
+                }
+            }
+    }
+
+    // 양방향 데이터 통신
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        super.handleTextMessage(session, message)
+    }
+
+    // 소켓 연결 종료
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        super.afterConnectionClosed(session, status)
+    }
+
+    // 소켓 통신 에러
+    override fun handleTransportError(session: WebSocketSession, exception: Throwable) {
+        super.handleTransportError(session, exception)
+    }
+
 }

41e1b03426650e6858c1ed47bfd5da7f6839d405
전승원 <78971821+louis7308@users.noreply.github.com>
2023-10-10 09:51:37 +0900

    [Add]: websocket 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     // DB
-    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // websocket
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")

466ba8f0e2e893223b4683a4d12d2b16805ef51c
전승원 <78971821+louis7308@users.noreply.github.com>
2023-10-17 23:35:38 +0900

    [Fix]: /data 폴더 .gitignore 처리 안되는거 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,19 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     // DB
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation ("org.redisson:redisson-spring-boot-starter:3.18.0")
 
     // websocket
     implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // mysql
+    runtimeOnly ("mysql:mysql-connector-java")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

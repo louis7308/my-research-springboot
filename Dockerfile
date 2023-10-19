cd2f95c48c16fc6cf09fb5310d8d15f2424f4907
전승원 <78971821+louis7308@users.noreply.github.com>
2023-10-19 11:32:46 +0900

    [Fix]: Dockerfile 에서 prod 운영 환경 yml에 맞게 수정

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ ARG JAR_FILE=/build/libs/*.jar
 
 COPY ${JAR_FILE} /startspring.jar
 
-ENTRYPOINT ["java","-jar","-Dspring.profiles.active=prod", "/startspring.jar"]
+ENTRYPOINT ["java","-jar", "/startspring.jar"]
--- src/main/kotlin/com/example/startspring/StartSpringApplication.kt
+++ src/main/kotlin/com/example/startspring/StartSpringApplication.kt
@@ -7,44 +7,7 @@ import org.springframework.boot.runApplication
 class StartSpringApplication
 
 fun main(args: Array<String>) {
-    var id_list = arrayOf("muzi", "frodo", "apeach", "neo")
-    var report = arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi")
-    println( solution(id_list, report, 2).toString())
     runApplication<StartSpringApplication>(*args)
-}
-
-fun solution(idList: Array<String>, report: Array<String>, k: Int): IntArray {
-    var answer: IntArray = IntArray(1000, {0})
-    // 신고 당한 횟수 Hash Map
-    var userReportHashCount = HashMap<String, Int>()
-    for(usr in idList) userReportHashCount.put(usr, 0)
-    // 중복 제거
-    var reportHashSet = HashSet<String>()
-    for(rep in report) reportHashSet.add(rep)
-
-
-    var userHash = HashMap<String, MutableList<String>>()
-    for(rep in reportHashSet) {
-        val t = rep.split(" ")
-        userHash.put(t[0], userHash.getOrDefault(t[0], mutableListOf(t[1])))?.add(t[1])
-        userReportHashCount.put(t[1], userReportHashCount.get(t[1])!! + 1)
-    }
-
-    var count = 0
-    for(key in userReportHashCount.keys) {
-        var value = userReportHashCount.get(key)
-        if(k <= value!!) {
-            for(userKey in userHash.keys) {
-                if(userHash.get(userKey)?.contains(key) == true) {
-                    answer[count] = answer[count] + 1
-                }
-            }
-        }
-    }
-
-
-    println(userHash)
-    return answer
 }
 
 
